4748643fcda5310d5b09abfad28a1863a73b3b8e
Trọng Nguyễn Đức <trongnguyen160902@gmail.com>
2023-08-25 21:17:55 +0700

    update test

--- src/components/layout/LayoutPage.tsx
+++ src/components/layout/LayoutPage.tsx
@@ -10,11 +10,11 @@ const LayoutPage = ({ children }: any) => {
     return (
         <div>
             <header className="bg-black p-6 text-[#fff]  flex gap-5">
-                <Link className="p-5 bg-[#ccc]" to='/'>home</Link>
+                {/* <Link className="p-5 bg-[#ccc]" to='/'>home</Link>
                 <Link className="p-5 bg-[#ccc] " to='/profile'>profile</Link>
                 <Link className="p-5 bg-[#ccc] " to='/login'>login</Link>
                 <Link className="p-5 bg-[#ccc] " to='/register'>register</Link>
-                <Link className="p-5 bg-[#ccc] " to='/searchResults'>kêt quả tìm kiếm</Link>
+                <Link className="p-5 bg-[#ccc] " to='/searchResults'>kêt quả tìm kiếm</Link> */}
             </header>
             <div>
                 <Suspense fallback={<Spin className='w-full h-[80vh] flex items-center justify-center ' indicator={antIcon} />} >
